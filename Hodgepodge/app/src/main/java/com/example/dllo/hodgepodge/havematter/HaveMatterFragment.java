a2a00cf441ff12ca36101f317682a229d56f31c9
王振涛 <766422659@qq.com>
2016-12-20 16:39:59 +0800

    no message

--- Hodgepodge/app/src/main/java/com/example/dllo/hodgepodge/havematter/HaveMatterFragment.java
+++ Hodgepodge/app/src/main/java/com/example/dllo/hodgepodge/havematter/HaveMatterFragment.java
@@ -1,13 +1,19 @@
 package com.example.dllo.hodgepodge.havematter;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+
 import com.example.dllo.hodgepodge.R;
 import com.example.dllo.hodgepodge.base.BaseFragment;
 
 /**
  * Created by TaiF on 16/12/19.
  */
-    
+
 public class HaveMatterFragment extends BaseFragment {
+    private TabLayout tbHaveMatter;
+    private ViewPager vpHaveMatter;
+
     @Override
     protected int setLayout() {
         return R.layout.fragment_havematter;
@@ -15,12 +21,13 @@ public class HaveMatterFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-
+        tbHaveMatter = bindView(R.id.tb_havematter);
+        vpHaveMatter = bindView(R.id.vp_havematter);
     }
 
     @Override
     protected void initData() {
-
+        HaveMatterAdatper mAdapter = new HaveMatterAdatper();
     }
 
 
